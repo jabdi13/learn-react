50c4c838db7da929ff941f456f683b8a60aae784
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-01-12 14:00:23 -0600

    Update arrays state problem 3

---
+++ src/modules/mod1-interactivity/unity3-update-array-state/problem3/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import AddTodo from './AddTodo.js';
+import TaskList from './TaskList.js';
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Comprar leche', done: true },
+  { id: 1, title: 'Comer tacos', done: false },
+  { id: 2, title: 'Preparar té', done: false },
+];
+
+export default function TaskApp() {
+  const [todos, setTodos] = useState(
+    initialTodos
+  );
+
+  function handleAddTodo(title) {
+    setTodos(todos.concat([{
+      id: nextId++,
+      title,
+      done: false
+    }]))
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(todos.map(todo => {
+      if(todo.id === nextTodo.id) {
+        return nextTodo
+      }
+      return todo
+    }))
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter(todo => todo.id !== todoId))
+  }
+
+  return (
+    <>
+      <AddTodo
+        onAddTodo={handleAddTodo}
+      />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
+}
