f17a568f11c8c050cac5f97b8c9466c89de98198
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-01-12 00:29:42 -0600

    Update arrays state problem 2

---
+++ src/modules/mod1-interactivity/unity3-update-array-state/problem2/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Queso',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Espaguetis',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+
+  function handleDecreaseClick(productId) {
+    const productIdx = products.findIndex(product => product.id === productId)
+    if(products[productIdx].count - 1 < 1) {
+      return setProducts(products.filter(product => product.id !== productId))
+    }
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count - 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+          <button onClick={() => {
+            handleDecreaseClick(product.id)
+          }}>
+            –
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
