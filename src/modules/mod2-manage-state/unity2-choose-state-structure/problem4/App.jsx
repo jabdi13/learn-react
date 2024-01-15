759530ed3104333f59cdf97dcca101621735f940
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-01-14 23:29:27 -0600

    Choose state structure problem 4

---
+++ src/modules/mod2-manage-state/unity2-choose-state-structure/problem4/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { letters } from './data.js';
+import Letter from './Letter.js';
+
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState(
+    new Set()
+  );
+
+  const selectedCount = selectedIds.size;
+
+  function handleToggle(toggledId) {
+    // Crea una copia (para evitar la mutación).
+    const nextIds = new Set(selectedIds);
+    if (nextIds.has(toggledId)) {
+      nextIds.delete(toggledId);
+    } else {
+      nextIds.add(toggledId);
+    }
+    setSelectedIds(nextIds);
+  }
+
+  return (
+    <>
+      <h2>Buzón de entrada</h2>
+      <ul>
+        {letters.map(letter => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              selectedIds.has(letter.id)
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>
+            Has seleccionado {selectedCount} cartas
+          </b>
+        </p>
+      </ul>
+    </>
+  );
+}
