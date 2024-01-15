e0715f8322d9199506b7bc8f948647358a8d38e9
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-01-14 23:09:16 -0600

    Choose state structure problem 3

---
+++ src/modules/mod2-manage-state/unity2-choose-state-structure/problem3/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { initialLetters } from './data.js';
+import Letter from './Letter.js';
+
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [idHighlightedLetter, setIdHighlightedLetter] = useState(null);
+
+  function handleHover(letterId) {
+    setIdHighlightedLetter(letterId);
+  }
+
+  function handleStar(starred) {
+    setLetters(letters.map(letter => {
+      if (letter.id === starred.id) {
+        return {
+          ...letter,
+          isStarred: !letter.isStarred
+        };
+      } else {
+        return letter;
+      }
+    }));
+  }
+
+  return (
+    <>
+      <h2>Buzón de entrada</h2>
+      <ul>
+        {letters.map(letter => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={
+              letter.id === idHighlightedLetter
+            }
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/modules/mod2-manage-state/unity2-choose-state-structure/problem3/Letter.jsx
@@ -0,0 +1,27 @@
+export default function Letter({
+  letter,
+  isHighlighted,
+  onHover,
+  onToggleStar,
+}) {
+  return (
+    <li
+      className={
+        isHighlighted ? 'highlighted' : ''
+      }
+      onFocus={() => {
+        onHover(letter.id);
+      }}
+      onPointerMove={() => {
+        onHover(letter.id);
+      }}
+    >
+      <button onClick={() => {
+        onToggleStar(letter);
+      }}>
+        {letter.isStarred ? 'Desmarcar' : 'Marcar'}
+      </button>
+      {letter.subject}
+    </li>
+  )
+}
