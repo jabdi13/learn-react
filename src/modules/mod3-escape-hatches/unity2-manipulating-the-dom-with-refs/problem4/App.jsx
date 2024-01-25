6c3560fbe868094006fa6adf1127904ad210816d
Abdiel Ortega <nathanabdiel1@gmail.com>
2024-01-24 18:36:21 -0600

    Manipulating the dom with refs problem 4

---
+++ src/modules/mod3-escape-hatches/unity2-manipulating-the-dom-with-refs/problem4/App.jsx
@@ -0,0 +1,17 @@
+import { useRef } from 'react';
+import SearchButton from './SearchButton.js';
+import SearchInput from './SearchInput.js';
+
+export default function Page() {
+  const inputRef = useRef(null);
+  return (
+    <>
+      <nav>
+        <SearchButton onClick={() => {
+          inputRef.current.focus();
+        }} />
+      </nav>
+      <SearchInput ref={inputRef} />
+    </>
+  );
+}
